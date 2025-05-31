6ca2b06464e5c2a41e4cf6116bdc08ced96a22d9
Adam Lemmo <gigazarak@gmail.com>
2025-05-31 15:00:16 +1000

    changes

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ if ("serviceWorker" in navigator) {
     .then(() => console.log("Service Worker registered"))
     .catch((err) => console.error("Service Worker error:", err));
 }
-const url = new URL(window.location);
-const sharedTitle = url.searchParams.get("title");
-const sharedText = url.searchParams.get("text");
-const sharedUrl = url.searchParams.get("url");
+// const url = new URL(window.location);
+// const sharedTitle = url.searchParams.get("title");
+// const sharedText = url.searchParams.get("text");
+// const sharedUrl = url.searchParams.get("url");
 
-// Use the shared data to update your PWA's UI or perform actions
-console.log("Shared Title:", sharedTitle);
-console.log("Shared Text:", sharedText);
-console.log("Shared URL:", sharedUrl);
+// // Use the shared data to update your PWA's UI or perform actions
+// console.log("Shared Title:", sharedTitle);
+// console.log("Shared Text:", sharedText);
+// console.log("Shared URL:", sharedUrl);
 
-const userInputDiv = document.getElementById("userInput");
-userInputDiv.textContent = sharedTitle;
+// const userInputDiv = document.getElementById("userInput");
+// userInputDiv.textContent = sharedTitle;
 // Show missing letters and reveal the copy button
 document.getElementById("userInputForm").addEventListener("submit", function (event) {
   event.preventDefault();
