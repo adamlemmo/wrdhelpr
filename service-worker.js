1f35dc072107ff99c1ee154ec32c683eb9e6d469
Adam Lemmo <adam.lemmo@monash.edu>
2025-06-28 12:08:18 +1000

    added service worker js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'wrdhelpr-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',            // index.html root
+  '/index.html',
+  '/manifest.json',
+  '/style.css',   // or your CSS file(s)
+  '/main.js',     // or your JS file(s)
+  '/pwa-192x192.png',
+  '/pwa-512x512.png',
+  // Add other assets you want cached
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) =>
+      cachedResponse || fetch(event.request)
+    )
+  );
+});
