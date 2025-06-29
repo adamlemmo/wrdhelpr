efe9ddc97f7fd16a58bf456efe06dfdc77e94af7
Adam Lemmo <adam.lemmo@monash.edu>
2025-06-29 12:18:36 +1000

    updated bugs

--- vite.config.js
+++ vite.config.js
@@ -2,51 +2,59 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: 'auto',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      },
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: "wrdhelpr",
-        short_name: "wrdhelpr",
-        scope: '.',
-        start_url: ".",
-        theme_color: "#317EFB",
-        icons: [
-          {
-            src: "icons/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "icons/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-        "share_target": {
-          "action": "/share-target.html",
-          "method": "GET",
-          "enctype": "application/x-www-form-urlencoded",
-          "params": {
-            "title": "title",
-            "text": "text",
-            "url": "url"
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        injectRegister: 'auto',
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        },
+        devOptions: {
+          enabled: true
+        },
+        manifest: {
+          name: "wrdhelpr",
+          short_name: "wrdhelpr",
+          scope: '/wrdhelpr/',
+          start_url: "/wrdhelpr/",
+          theme_color: "#317EFB",
+          icons: [
+            {
+              src: "icons/pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "icons/pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+          "share_target": {
+            "action": "/share-target.html",
+            "method": "GET",
+            "enctype": "application/x-www-form-urlencoded",
+            "params": {
+              "title": "title",
+              "text": "text",
+              "url": "url"
+            }
           }
-        }
-      },
-    }),
-  ],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-  base: '',
+        },
+      }),
+    ],
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+    },
+    base: '/',
+  };
+
+  if (command === 'build') {
+    config.base = '/wrdhelpr/';
+  }
+
+  return config;
 });
